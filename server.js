53f886982219cac7a401b4cde8602e68afe610e0
Prince Lee <prince_lee.nna85@ymail.com>
2024-10-09 15:00:14 +0100

    feature:add cor and welcome message

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const sequelize = require('./config/database');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
+// Define the root route
+app.get('/', (req, res) => {
+    res.send('Welcome to backend');
+});
+
 app.use('/api/auth', authRoutes);
+
 
 // Sync models and start the server
 sequelize.sync().then(() => {
